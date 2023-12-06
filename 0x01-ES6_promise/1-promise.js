06f559c48bb4219ef121165beb4711440de31318
nja93 <njanjalchege@gmail.com>
2023-12-06 20:53:31 +0300

    fixed eslint comment

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,5 +1,7 @@
 export default function getFullResponseFromAPI(success) {
+  /* promise constructor arrow fn takes in reject and resolve */
   return new Promise((resolve, reject) => {
+    /* if successfull, which is true, returns success obj, else, error meesage */
     if (success)resolve({ status: 200, body: 'Success' });
     else reject(Error('The fake API is not working currently'));
   });
