9d603c9752eaa8d887a9611c09be177794a4f9ce
nja93 <njanjalchege@gmail.com>
2023-12-06 18:31:26 +0300

    create a promise that queries api

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,7 @@
-export default function getResponseFromAPI(){
- return new Promise((resolve, reject) => {});
+export default function getResponseFromAPI() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('API response');
+    }, 1000);
+  });
 }
